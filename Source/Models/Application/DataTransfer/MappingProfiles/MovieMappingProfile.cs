1f1d096fa09565097a8e4fd405bf27795226d8b2
Santiago Florez <santiagoo98p@gmail.com>
2023-03-17 21:45:15 -0300

    fix: poster not mapping

--- Source/Models/Application/DataTransfer/MappingProfiles/MovieMappingProfile.cs
+++ Source/Models/Application/DataTransfer/MappingProfiles/MovieMappingProfile.cs
@@ -8,15 +8,15 @@ namespace TDP.Models.Application.DataTransfer.MappingProfiles
         public MovieMappingProfile()
         {
             // TODO: use single CreateMap.
-            CreateMap<TDP.Models.Domain.Movie, Movie>().ForMember(dto => dto.Id, src => src.MapFrom(src => src.Id));
-            CreateMap<TDP.Models.Domain.Movie, Movie>().ForMember(dto => dto.Title, src => src.MapFrom(src => src.Title));
-            CreateMap<TDP.Models.Domain.Movie, Movie>().ForMember(dto => dto.Plot, src => src.MapFrom(src => src.Plot));
-            CreateMap<TDP.Models.Domain.Movie, Movie>().ForMember(dto => dto.Runtime, src => src.MapFrom(src => src.Runtime));
-            CreateMap<TDP.Models.Domain.Movie, Movie>().ForMember(dto => dto.Type, src => src.MapFrom(src => src.Type));
-            CreateMap<TDP.Models.Domain.Movie, Movie>().ForMember(dto => dto.Released, src => src.MapFrom(src => src.Released));
-            CreateMap<TDP.Models.Domain.Movie, Movie>().ForMember(dto => dto.Poster, src => src.MapFrom(src => src.PosterUrl));
-            CreateMap<TDP.Models.Domain.Movie, Movie>().ForMember(dto => dto.Country, src => src.MapFrom(src => src.Country));
-            CreateMap<TDP.Models.Domain.Movie, Movie>().ForMember(dto => dto.imdbRating, src => src.MapFrom(src => src.ImdbRating));
+            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Id, src => src.MapFrom(src => src.Id));
+            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Title, src => src.MapFrom(src => src.Title));
+            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Plot, src => src.MapFrom(src => src.Plot));
+            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Runtime, src => src.MapFrom(src => src.Runtime));
+            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Type, src => src.MapFrom(src => src.Type));
+            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Released, src => src.MapFrom(src => src.Released));
+            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Country, src => src.MapFrom(src => src.Country));
+            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.imdbRating, src => src.MapFrom(src => src.ImdbRating));
+            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Poster, src => src.MapFrom(src => src.PosterUrl));
         }
     }
 }
