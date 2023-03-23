20e79f675ea380fb28fb2043e3b54ce86170bd65
MatiasQuaroni <matiasquaroni98@gmail.com>
2023-03-23 17:50:54 -0300

    fix: moviemapping format

--- Source/Models/Application/DataTransfer/MappingProfiles/MovieMappingProfile.cs
+++ Source/Models/Application/DataTransfer/MappingProfiles/MovieMappingProfile.cs
@@ -9,18 +9,13 @@ namespace TDP.Models.Application.DataTransfer.MappingProfiles
         public MovieMappingProfile()
         {
             // TODO: use single CreateMap.
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Id, src => src.MapFrom(src => src.Id));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Title, src => src.MapFrom(src => src.Title));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Plot, src => src.MapFrom(src => src.Plot));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Runtime, src => src.MapFrom(src => src.Runtime));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Type, src => src.MapFrom(src => src.Type));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Released, src => src.MapFrom(src => src.Released));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Country, src => src.MapFrom(src => src.Country));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.imdbRating, src => src.MapFrom(src => src.ImdbRating));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Poster, src => src.MapFrom(src => src.PosterUrl));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Actors, src => src.MapFrom(src => string.Join(", ", src.Participants.Where(p => p.Role == (ParticipantRole)0).Select(part => part.Name))));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Director, src => src.MapFrom(src => string.Join(", ", src.Participants.Where(p => p.Role == (ParticipantRole)1).Select(part => part.Name))));
-            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Writer, src => src.MapFrom(src => string.Join(", ", src.Participants.Where(p => p.Role == (ParticipantRole)2).Select(part => part.Name))));
+            CreateMap<Domain.Movie, Movie>().ForMember(dto => dto.Id, src => src.MapFrom(src => src.Id)).ForMember(dto => dto.Title, src => src.MapFrom(src => src.Title)).
+                ForMember(dto => dto.Plot, src => src.MapFrom(src => src.Plot)).ForMember(dto => dto.Runtime, src => src.MapFrom(src => src.Runtime)).
+                ForMember(dto => dto.Type, src => src.MapFrom(src => src.Type)).ForMember(dto => dto.Released, src => src.MapFrom(src => src.Released)).
+                ForMember(dto => dto.Country, src => src.MapFrom(src => src.Country)).ForMember(dto => dto.imdbRating, src => src.MapFrom(src => src.ImdbRating)).
+                ForMember(dto => dto.Poster, src => src.MapFrom(src => src.PosterUrl)).ForMember(dto => dto.Actors, src => src.MapFrom(src => string.Join(", ", src.Participants.Where(p => p.Role == (ParticipantRole)0).Select(part => part.Name)))).
+                ForMember(dto => dto.Director, src => src.MapFrom(src => string.Join(", ", src.Participants.Where(p => p.Role == (ParticipantRole)1).Select(part => part.Name)))).
+                ForMember(dto => dto.Writer, src => src.MapFrom(src => string.Join(", ", src.Participants.Where(p => p.Role == (ParticipantRole)2).Select(part => part.Name))));
 
 
 
